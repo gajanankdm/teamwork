9a3fdda9a05c1ae2ce7da07bc9ff2eb792a4ab8c
jrahul12 <rj0093716@gmail.com>
2025-12-31 21:22:35 +0530

    created add and remove

--- src/app/shared/component/student/student.component.ts
+++ src/app/shared/component/student/student.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Istudent } from '../const/type';
+import { studentArr } from '../../model/data';
 
 @Component({
   selector: 'app-student',
@@ -8,20 +10,38 @@ import { Component, OnInit } from '@angular/core';
 export class StudentComponent {
 
   constructor() { }
-studentArr = [
-  {
-    fname: 'Gajanan',
-    lname: 'Kadam',
-    email: 'gajanan@gmail.com',
-    contact: '9876543210'
-  },
-  {
-    fname: 'Rahul',
-    lname: 'Patil',
-    email: 'rahul@gmail.com',
-    contact: '9123456780'
+  studentArr: Istudent[] = studentArr
+  @ViewChild('fname') fname!: ElementRef<HTMLInputElement>
+  @ViewChild('lname') lname!: ElementRef<HTMLInputElement>
+  @ViewChild('email') email!: ElementRef<HTMLInputElement>
+  @ViewChild('contact') contact!: ElementRef<HTMLInputElement>
+  onAdd(): void {
+    let fname = this.fname.nativeElement.value;
+    let lname = this.lname.nativeElement.value;
+    let email = this.email.nativeElement.value;
+    let contact = this.contact.nativeElement.value;
+    if (!fname || !lname || !email || !contact) return;
+    let createObj: Istudent = {
+      fname: fname,
+      lname: lname,
+      email: email,
+      contact: contact, 
+      id: Date.now().toString()
+    }
+    studentArr.unshift(createObj)
+    this.fname.nativeElement.value=""
+    this.lname.nativeElement.value=""
+    this.email.nativeElement.value=""
+    this.contact.nativeElement.value=""
   }
-];
+  onRemove(id:string){
+    let value=studentArr.findIndex(s=>s.id===id);
+    if (value>-1) {
+      studentArr.splice(value,1)
+    }
+  }
+  onEdit(st: Istudent): void {
 
+  }
 
 }
